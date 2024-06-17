317cf55dab5b2f94aac55fa3d6679fe6d19b289c
risadniati <152244838+risadniati@users.noreply.github.com>
2024-06-17 15:00:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -13,13 +13,15 @@ curricular_units_1st_sem_approved = st.number_input("Curricular Units 1st Sem Ap
 curricular_units_1st_sem_grade = st.number_input("Curricular Units 1st Sem Grade", min_value=0.0, max_value=20.0, value=0.0)
 curricular_units_2nd_sem_approved = st.number_input("Curricular Units 2nd Sem Approved", min_value=0, max_value=100, value=0)
 curricular_units_2nd_sem_grade = st.number_input("Curricular Units 2nd Sem Grade", min_value=0.0, max_value=20.0, value=0.0)
+tuition_fees_up_to_date = st.selectbox("Tuition Fees Up-to-date(1 = Yes, 0 = No)", [0, 1])
 
 # Predict button
 if st.button("Predict"):
     features = np.array([[curricular_units_1st_sem_approved, 
                           curricular_units_1st_sem_grade,
                           curricular_units_2nd_sem_approved, 
-                          curricular_units_2nd_sem_grade]])
+                          curricular_units_2nd_sem_grade, 
+                          tuition_fees_up_to_date]])
     
     prediction = model.predict(features)
     prediction_proba = model.predict_proba(features)
